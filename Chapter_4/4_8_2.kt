9fd40d331391c5b0d2b6f5df0cd49ad49501aad4
blueice12138 <838343608@qq.com>
2020-07-28 00:23:08 +0800

    Add seal class in adapter

---
+++ Chapter_4/4_8_2.kt
@@ -0,0 +1,10 @@
+sealed class Result
+class Success(val msg: String) : Result()
+class Failure(val error: Exception) : Result()
+fun getResultMsg(result: Result) = when (result) {
+    is Success -> result.msg
+    is Failure -> result.error.message
+}
+fun main() {
+    
+}
--- Chapter_4/UIBestPractice/app/src/main/java/com/example/uibestpractice/MsgAdapter.kt
+++ Chapter_4/UIBestPractice/app/src/main/java/com/example/uibestpractice/MsgAdapter.kt
@@ -6,20 +6,28 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class MsgAdapter(val msgList: List<Msg>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    inner class LeftViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val leftMsg: TextView = view.findViewById(R.id.leftMsg)
-    }
-    inner class RightViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val rightMsg: TextView = view.findViewById(R.id.rightMsg)
-    }
+sealed class MsgViewHolder(view: View) : RecyclerView.ViewHolder(view)
+class LeftViewHolder(view: View) : MsgViewHolder(view) {
+    val leftMsg: TextView = view.findViewById(R.id.leftMsg)
+}
+class RightViewHolder(view: View) : MsgViewHolder(view) {
+    val rightMsg: TextView = view.findViewById(R.id.rightMsg)
+}
+
+class MsgAdapter(val msgList: List<Msg>) : RecyclerView.Adapter<MsgViewHolder>() {
+//    inner class LeftViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+//        val leftMsg: TextView = view.findViewById(R.id.leftMsg)
+//    }
+//    inner class RightViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+//        val rightMsg: TextView = view.findViewById(R.id.rightMsg)
+//    }
 
     override fun getItemViewType(position: Int): Int {
         val msg = msgList[position]
         return msg.type
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MsgViewHolder {
         if (viewType == Msg.TYPE_RECEIVED) {
             val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.msg_left_item, parent, false)
@@ -31,12 +39,11 @@ class MsgAdapter(val msgList: List<Msg>) : RecyclerView.Adapter<RecyclerView.Vie
         }
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MsgViewHolder, position: Int) {
         val msg = msgList[position]
         when (holder) {
             is LeftViewHolder -> holder.leftMsg.text = msg.content
             is RightViewHolder -> holder.rightMsg.text = msg.content
-            else -> throw IllegalArgumentException()
         }
     }
 
